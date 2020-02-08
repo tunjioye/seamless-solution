491cb79ac1d16b206daaeb4a7be0d2effe9314af
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2020-02-08 10:34:40 +0100

    Updates

---
+++ precache-manifest.70a4212f75af06d4971e2882d9acf099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a384b7300f73d65360757814041cda",
+    "url": "/seamless-solution/index.html"
+  },
+  {
+    "revision": "7581a241bba85c315625",
+    "url": "/seamless-solution/static/css/main.4c837e1a.chunk.css"
+  },
+  {
+    "revision": "1c58a5de022daba16bf8",
+    "url": "/seamless-solution/static/js/2.13aa99f5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/seamless-solution/static/js/2.13aa99f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7581a241bba85c315625",
+    "url": "/seamless-solution/static/js/main.2a8965e1.chunk.js"
+  },
+  {
+    "revision": "fcee5e331b42b70d84f1",
+    "url": "/seamless-solution/static/js/runtime-main.9eccd825.js"
+  }
+]);
